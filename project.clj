57e980340f2db064ef86858c784f79aa6a645f1b
eidens <eidens@github>
2015-04-13 22:15:48 +0200

    added :scm to project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,9 @@
 (defproject eidens/markov "0.1.0-SNAPSHOT"
   :description "markov matrices in clojure"
-  :url "https://github.com/eidens/markov"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :url "https://github.com/eidens/markov"
+  :scm {:url "git@github.com:eidens/markov.git"}
+
   :dependencies [[org.clojure/clojure "1.6.0"]])
